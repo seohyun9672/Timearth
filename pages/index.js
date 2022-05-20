83eab515347f8088eae438d63d48d514ddc5fd1a
seohyun9672 <ajeong4@my.bcit.ca>
2022-05-20 05:23:43 -0700

    update page

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,22 @@ import { useRouter } from "next/router";
 import styled from 'styled-components';
 import { PageTitle, BtnText } from '../data/index_content'
 import { Hamburger, Menu } from "../comps/Menu";
-import React, { useState, useRef } from "react";
+import React from "react";
 import NavBar from "../comps/NavBar";
 import { logoData } from "../data/global_content";
+// import Image from 'next/image';
+
 
 const LayoutComp = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    position: absolute;
-    top: 40%;
-    transform: translate(0, -40%);
-    width: 90%;
+    margin-top: 10em;
+
+    @media only screen and (max-width: 320px) and (max-height: 800px){
+        margin-top: 7em;
+    }
 `
 
 const Em = styled.span`
@@ -26,6 +29,7 @@ const Em = styled.span`
 const Logo = styled.img`
     width: 40%;
     height: 40%;
+
     @media only screen and (max-width: 320px) and (max-height: 800px){
         width: 25%;
         height: 25%;
@@ -38,26 +42,28 @@ const Logo = styled.img`
         width: 30%;
         height: 30%;
     }
+
 `
 
 export default function Home() {
 
-    const [open, setOpen] = useState(false);
-    const node = useRef();
+    const [open, setOpen] = React.useState(false);
+    const node = React.useRef();
 
     const r = useRouter();
     return (
-        <div className="setting">
+        <LayoutComp>
             <NavBar />
             <div className="menu" ref={node}>
                 <Hamburger open={open} setOpen={setOpen} />
                 <Menu open={open} setOpen={setOpen} />
             </div>
-            <LayoutComp>
-                <Logo src={logoData.src} alt="Timearth Logomark" />
-                <h1>{PageTitle}</h1>
-                <h4>Log your daily activities and see how many <Em>Earth minutes</Em> you have saved today!</h4>
-            </LayoutComp>
+            <Logo
+                src= {logoData.src}
+                alt="Timearth Logomark"
+               />
+            <h1>{PageTitle}</h1>
+            <h4>Log your daily activities and see how many <Em>Earth minutes</Em> you have saved today!</h4>
             <div className="background-shape" />
             <button
                 className="default"
@@ -71,6 +77,6 @@ export default function Home() {
                     }
                 }>{BtnText}
             </button>
-        </div>
+        </LayoutComp>
     )
 }
